7cf1ab990268a5cc671509e7d30a4be9d8e47c3f
seylorx1 <61364803+seylorx1@users.noreply.github.com>
2022-01-07 02:02:05 +0000

    Added glass footstep noises to Wwise

--- Unity Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Unity Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -159,6 +159,7 @@ namespace AK
             {
                 static const AkUniqueID BED = 446279778U;
                 static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID GLASS = 2449969375U;
                 static const AkUniqueID GRASS = 4248645337U;
                 static const AkUniqueID GRAVEL = 2185786256U;
                 static const AkUniqueID METAL = 2473969246U;
